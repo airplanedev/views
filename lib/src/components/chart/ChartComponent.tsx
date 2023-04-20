d4725e8f69ec28d88643d8e37180f1805e5ae777
Lee Weisberger <lee@airplane.dev>
2023-04-20 14:01:59 -0400

    Add transition

--- lib/src/components/chart/ChartComponent.tsx
+++ lib/src/components/chart/ChartComponent.tsx
@@ -1,4 +1,4 @@
-import { createStyles } from "@mantine/core";
+import { createStyles, Transition } from "@mantine/core";
 import Plotly, { Data } from "plotly.js-basic-dist";
 import createPlotlyComponent from "react-plotly.js/factory";
 
@@ -60,49 +60,6 @@ const ChartComponent = ({
   // Layout controls the visual aspects of the chart.
   const layout = buildLayout(restProps);
 
-  let component = (
-    <Plot
-      onDeselect={onDeselect}
-      onSelected={onSelected}
-      data={normalizedData}
-      layout={layout}
-      useResizeHandler
-      config={{
-        modeBarButtonsToRemove: ["select2d", "lasso2d"],
-      }}
-      className={classes.plot}
-    />
-  );
-  if (loading) {
-    component = (
-      <div style={{ marginTop: 10, overflow: "hidden" }}>
-        {new Array(NUM_LOADING_LINES).fill(0).map((_, index) => (
-          <Skeleton
-            key={index}
-            height={2}
-            // If the container's width is set, fill it. If not, use a fixed width.
-            width={width != null ? "100%" : 500}
-            mb={index < NUM_LOADING_LINES - 1 ? 50 : 0}
-          />
-        ))}
-      </div>
-    );
-  }
-  if (error) {
-    component = (
-      <Label
-        style={{
-          // If the container's width/height is set, fill it. If not, use a fixed width/height.
-          width: width != null ? "100%" : 500,
-          height: height != null ? "100%" : 200,
-        }}
-        color="red"
-      >
-        {error}
-      </Label>
-    );
-  }
-
   return (
     <div style={style} className={cx(classes.wrapper, layoutClasses.style)}>
       {
@@ -113,7 +70,52 @@ const ChartComponent = ({
           </Heading>
         ) : null
       }
-      {component}
+      {loading && (
+        <div style={{ marginTop: 10, overflow: "hidden" }}>
+          {new Array(NUM_LOADING_LINES).fill(0).map((_, index) => (
+            <Skeleton
+              key={index}
+              height={2}
+              // If the container's width is set, fill it. If not, use a fixed width.
+              width={width != null ? "100%" : 500}
+              mb={index < NUM_LOADING_LINES - 1 ? 50 : 0}
+            />
+          ))}
+        </div>
+      )}
+      {error && (
+        <Label
+          style={{
+            // If the container's width/height is set, fill it. If not, use a fixed width/height.
+            width: width != null ? "100%" : 500,
+            height: height != null ? "100%" : 200,
+          }}
+          color="red"
+        >
+          {error}
+        </Label>
+      )}
+      <Transition
+        mounted={!loading && !error}
+        transition="pop-bottom-left"
+        duration={1000}
+        timingFunction="ease"
+      >
+        {(styles) => (
+          <Plot
+            style={styles}
+            onDeselect={onDeselect}
+            onSelected={onSelected}
+            data={normalizedData}
+            layout={layout}
+            useResizeHandler
+            config={{
+              modeBarButtonsToRemove: ["select2d", "lasso2d"],
+            }}
+            className={classes.plot}
+          />
+        )}
+      </Transition>
     </div>
   );
 };
